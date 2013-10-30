7fe11ab423515912de2efbc6c9b623742d86f265
SharangP <sharang.phadke@gmail.com>
2013-10-29 21:23:05 -0400

    finalized the othello.cpp structure, need to add things to game.Start

--- game.cpp
+++ game.cpp
@@ -96,7 +96,16 @@ void Game::randomMove(){
 }
 
 void Game::Start(){
-    //while loop
+//    while(true){
+//        int movenum = 0;
+//        vector<Board::Move> m = b->LegalMoves(WHITE);
+//        for(int i = 0; i < m.size(); i++)
+//            cout << i << ": [" << (int)m[i].square.y << "," << (int)m[i].square.x << "]" << endl;
+//        cout << "choose a move: ";
+//        cin >> movenum;
+//        b->ApplyMove(m[movenum]);
+//        b->Print();
+//    }
 }
 
 #endif //_GAME_CPP_
--- othello.cpp
+++ othello.cpp
@@ -17,23 +17,18 @@ using namespace std;
 int main(int argc, char *argv[]){
     int gameType;
 
-    cout << "Welcome to Sharang's Othello AI" << endl
-         << "Choose a game type:" << endl
-         << "1: go first (black)" << endl
-         << "2. go second (white)" << endl
-         << "3: computer vs. computer" << endl;
-    cin >> gameType;
-    Game game = Game(gameType);
-//    Board *b = new Board();
-//    b->Print();
-//    while(true){
-//        int movenum = 0;
-//        vector<Board::Move> m = b->LegalMoves(WHITE);
-//        for(int i = 0; i < m.size(); i++)
-//            cout << i << ": [" << (int)m[i].square.y << "," << (int)m[i].square.x << "]" << endl;
-//        cout << "choose a move: ";
-//        cin >> movenum;
-//        b->ApplyMove(m[movenum]);
-//        b->Print();
-//    }
+    while(true){
+        cout << "Welcome to Sharang's Othello AI" << endl
+             << "Choose a game type:" << endl
+             << "1: go first (black)" << endl
+             << "2. go second (white)" << endl
+             << "3: computer vs. computer" << endl;
+        cin >> gameType;
+        if((gameType == 1) || (gameType == 2) || (gameType == 3)){
+            Game game = Game(gameType);
+            break;
+        }
+        cout << "Invalid option!" << endl;
+    }
+    game.Start();
 }
